045bc98b6e54fe62d30920ef08a8eed462f62115
Roberto-Aguirre <ivanaguirre02@hotmail.com>
2024-07-13 01:57:27 -0600

    Crear modelos de direccion y medico

---
+++ src/main/java/med/voll/api/models/medico/Medico.java
@@ -0,0 +1,35 @@
+package med.voll.api.models.medico;
+import jakarta.persistence.Embedded;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import med.voll.api.models.direccion.Direccion;
+
+@Entity(name = "medico")
+@Table(name = "medicos")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Medico {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nombre;
+    private String email;
+    private String documentoId;
+    @Enumerated(EnumType.STRING)
+    private Especialidad especialidad;
+    @Embedded
+    private Direccion datosDireccion;
+}
