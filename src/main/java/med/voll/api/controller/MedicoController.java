b0a26710dee0261acc6eee7485a839c21882e3d4
Roberto-Aguirre <ivanaguirre02@hotmail.com>
2024-07-13 20:10:03 -0600

    Modificaciones menores de lineas

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -9,9 +9,7 @@ import med.voll.api.models.medico.DatosListadoMedico;
 import med.voll.api.models.medico.DatosRegistroMedico;
 import med.voll.api.models.medico.Medico;
 import med.voll.api.models.medico.MedicoRepository;
-
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -22,9 +20,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMethod;
-
-
 
 
 
